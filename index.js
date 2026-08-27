44df05e760db866882b041fbe09284b264bdbc29
yoshimitendo <vk6sgysw7p@privaterelay.appleid.com>
2026-08-27 21:32:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const problem = document.getElementById("problem");
+const gamelayer = document.getElementById("GameLayer");
 
 problem.textContent = "サッカー場に向かって走っている男の子を私は見た。";
+
+const PIECES = ["I", "saw", "a", "man", "running", "toward", "the", "statiton"];
+
+PIECES.forEach((p) => {
+    const piece = document.createElement("div");
+    piece.className = "pieces"
+    piece.textContent = p;
+
+    piece.style.left = `${Math.random() * 100}%`;
+    piece.style.top = `${Math.random() * 100}%`;
+
+    gamelayer.appendChild(piece);
+})
