c225130919b4e8ec52653737788294ff06353ffe
yoshimitendo <vk6sgysw7p@privaterelay.appleid.com>
2026-08-30 00:03:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ document.addEventListener("dblclick", (e) => {
     e.preventDefault();
 }, {passive: false})
 
-const problem = document.getElementById("problem");
-const gamelayer = document.getElementById("GameLayer");
-
-problem.textContent = "私は駅に向かって走っている男性を見た。";
-
-const PIECES = ["I", "saw", "a", "man", "rdocument.addEventListener("dblclick", (e) => {
-    e.preventDefault();
-}, {passive: false})
-
 const problemlayer = document.getElementById("problem");
 const gamelayer = document.getElementById("GameLayer");
 
@@ -71,49 +62,6 @@ PIECES.forEach((p) => {
     piece.addEventListener("pointerup", (e) => {
         e.preventDefault();
         piece.style.background = piece.BGcolor;
-        piece.isPointer = false;
-
-        piece.releasePointerCapture(e.pointerId);
-    })
-
-    piece.addEventListener("pointermove", (e) => {
-        e.preventDefault();
-        if (!piece.isPointer) return;
-        piece.X = e.clientX - piece.pointerX;
-        piece.Y = e.clientY - piece.pointerY;
-        piece.style.left = `${piece.X}px`;
-        piece.style.top = `${piece.Y}px`;
-    })
-})unning", "toward", "the", "station"];
-
-PIECES.forEach((p) => {
-    const piece = document.createElement("div");
-    piece.className = "pieces";
-    piece.textContent = p;
-
-    gamelayer.appendChild(piece);
-
-    const rect = gamelayer.getBoundingClientRect();
-    piece.X = Math.random() * (rect.width - 100);
-    piece.Y = Math.random() * (rect.height - 100);
-    piece.style.left = `${piece.X}px`;
-    piece.style.top = `${piece.Y}px`;
-
-    piece.isPointer = false;
-
-    piece.addEventListener("pointerdown", (e) => {
-        e.preventDefault();
-        piece.isPointer = true;
-        piece.style.background = "#275470";
-        piece.pointerX = e.clientX - piece.X;
-        piece.pointerY = e.clientY - piece.Y;
-        
-        piece.setPointerCapture(e.pointerId);
-    })
-
-    piece.addEventListener("pointerup", (e) => {
-        e.preventDefault();
-        piece.style.background = "#4987ae";
         piece.isPointer = false;
 
         piece.releasePointerCapture(e.pointerId);
