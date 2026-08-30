10216068ebd4b85378d9585cb0b692e5a1503c7f
yoshimitendo <vk6sgysw7p@privaterelay.appleid.com>
2026-08-31 01:35:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ document.addEventListener("dblclick", (e) => {
     e.preventDefault();
 }, {passive: false})
 
-const problemlayer = document.getElementById("problem");
-const gamelayer = document.getElementById("GameLayer");
+const problemLayer = document.getElementById("problem");
+const selectLayer = document.getElementById("select");
 
 const problem = "私は駅に向かって走っている男性を見た。";
-problemlayer.textContent = problem;
+problemLayer.textContent = problem;
 
 const PIECES = [
     {text: "I", pos: "pronoun"},
@@ -32,27 +32,29 @@ const POS = {
     "interjection": {tag: "間投詞", col: "#6d5aae"}
 };
 
+const Selection = [];
+
 PIECES.forEach((p) => {
     const piece = document.createElement("div");
     piece.className = "pieces";
     piece.textContent = p.text;
-    piece.BGcolor = POS[p.pos].col
-    piece.style.background = piece.BGcolor;
-
-    gamelayer.appendChild(piece);
-
-    const rect = gamelayer.getBoundingClientRect();
-    piece.X = Math.random() * (rect.width - 100);
-    piece.Y = Math.random() * (rect.height - 100);
-    piece.style.left = `${piece.X}px`;
-    piece.style.top = `${piece.Y}px`;
-
+    piece.style.background = POS[p.pos].col;
     piece.isPointer = false;
+    selectLayer.appendChild(piece);
 
+    const pieceRect = piece.getBoundingClientRect();
+    piece.gameWidth = pieceRect.width;
+
+    const index = Math.round(Math.random() * Selection.length)
+    Selection.splice(index, 0, piece)
+
+    linePiece();
+    
     piece.addEventListener("pointerdown", (e) => {
         e.preventDefault();
         piece.isPointer = true;
-        piece.style.background = "#cfcfcf";
+        piece.style.filter = "brightness(1.5)";
+        piece.style.zIndex = "1";
         piece.pointerX = e.clientX - piece.X;
         piece.pointerY = e.clientY - piece.Y;
         
@@ -61,7 +63,8 @@ PIECES.forEach((p) => {
 
     piece.addEventListener("pointerup", (e) => {
         e.preventDefault();
-        piece.style.background = piece.BGcolor;
+        piece.style.filter = "brightness(1.0)";
+        piece.style.zIndex = "0";
         piece.isPointer = false;
 
         piece.releasePointerCapture(e.pointerId);
@@ -76,3 +79,21 @@ PIECES.forEach((p) => {
         piece.style.top = `${piece.Y}px`;
     })
 })
+
+function linePiece() {
+    let lineWidth = 5;
+    let lineHeight = 5;
+
+    const selectRect = selectLayer.getBoundingClientRect();
+    Selection.forEach((e) => {
+        if (lineWidth + e.gameWidth > selectRect.width) {
+            lineHeight += 50;
+            lineWidth = 5;
+        }
+        e.X = lineWidth;
+        e.Y = lineHeight;
+        e.style.left = `${e.X}px`;
+        e.style.top = `${e.Y}px`
+        lineWidth += e.gameWidth + 5;
+    });
+}
